f7899240584965fce7c8e6305a895a1479a5c7c1
Leel24 <31742854+Leel24@users.noreply.github.com>
2017-10-03 12:49:47 -0500

    Add files via upload

---
+++ ChangeMachine.java
@@ -0,0 +1,31 @@
+package num;
+
+import java.util.Scanner;
+
+public class ChangeMachine 
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Cost of Item: ");
+		double cost = scan.nextDouble();
+		System.out.println("Amount Paid: ");
+		double paid = scan.nextDouble();
+		double change = paid-cost+.0001;
+		System.out.println("Change Owed: " + change);
+		int dollars = (int)change;
+		change = (change-dollars)*100;
+		int quarters = (int)change/25;
+		change = change%25;
+		int dimes = (int)change/10;
+		change = change%10;
+		int nickels = (int)change/5;
+		int pennies = (int)change%5;
+		System.out.println("Dollars: " + dollars);				
+		System.out.println("Quarters: " + quarters);
+		System.out.println("Dimes: " + dimes);
+		System.out.println("Nickels: " + nickels);
+		System.out.println("Pennies: " + pennies);
+		
+	}
+}
---
+++ WeightedAverage.java
@@ -0,0 +1,45 @@
+package num;
+
+import java.util.Scanner;
+
+public class WeightedAverage 
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int test1, test2, quiz1, quiz2, quiz3;
+		double hw;
+		
+		System.out.println("Please enter your test grades");
+		System.out.println("Test 1:");
+		test1 = scan.nextInt();
+		System.out.println("Test 2:");
+		test2 = scan.nextInt();
+		
+		System.out.println("Please enter your quiz grades");
+		System.out.println("Quiz 1:");
+		quiz1 = scan.nextInt();
+		System.out.println("Quiz 2:");
+		quiz2 = scan.nextInt();
+		System.out.println("Quiz 3");
+		quiz3 = scan.nextInt();
+		
+		System.out.println("Please enter your homework average");
+		System.out.println("Homework:");
+		hw = scan.nextInt();
+		
+		double testAvg = (test1+test2)/2.0;
+		double quizAvg = (quiz1+quiz2+quiz3)/3.0;
+		
+		System.out.println("Test Average: " + testAvg);
+		System.out.println("Quiz Average: " + quizAvg);
+		System.out.println("Homework: " + hw);
+		System.out.println("Final Grade: " + (testAvg*.5+quizAvg*.3+hw*.2));
+		
+		
+		
+	}
+	
+	
+}
+
